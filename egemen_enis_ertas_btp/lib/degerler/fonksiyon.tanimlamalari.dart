0a0ef87ce9a9c288d3bedf85fa3e3784f01f2615
egemenenis <enis.egemen25@gmail.com>
2021-05-24 18:23:09 +0300

    bot

---
+++ egemen_enis_ertas_btp/lib/degerler/fonksiyon.tanimlamalari.dart
@@ -0,0 +1,17 @@
+class Spor {
+  String imageUrl;
+  String name;
+  String type;
+  List<String> startTimes;
+  int etki;
+  int kcal;
+
+  Spor({
+    this.imageUrl,
+    this.name,
+    this.type,
+    this.startTimes,
+    this.etki,
+    this.kcal,
+  });
+}
